942ec04b0d331aeb7f7ef47e60c7eb5a5ed22db4
Rhys Vickers <rs.vickers5@gmail.com>
2023-12-08 11:33:03 -0700

    model for keep

---
+++ server/Models/Profile.cs
@@ -0,0 +1,7 @@
+namespace KeeprFinal.Models;
+
+public class Profile : RepoItem<string>
+{
+    public string Name { get; set; }
+    public string Picture { get; set; }
+}
--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -1,8 +1,26 @@
-CREATE TABLE IF NOT EXISTS accounts(
-  id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
-  createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
-  updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
-  name varchar(255) COMMENT 'User Name',
-  email varchar(255) COMMENT 'User Email',
-  picture varchar(255) COMMENT 'User Picture'
-) default charset utf8 COMMENT '';
+CREATE TABLE
+    IF NOT EXISTS accounts(
+        id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
+        createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
+        updatedAt DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Last Update',
+        name varchar(255) COMMENT 'User Name',
+        email varchar(255) COMMENT 'User Email',
+        picture varchar(255) NOT NULL,
+        coverImg VARCHAR(2000) NOT NULL
+    ) default charset utf8 COMMENT '';
+
+DROP TABLE accounts;
+
+CREATE TABLE
+    IF NOT EXISTS keeps(
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        name VARCHAR(255) NOT NULL,
+        description VARCHAR(1000) NOT NULL,
+        img VARCHAR(1000) NOT NULL,
+        views INT NOT NULL,
+        kept INT DEFAULT 0,
+        creatorId VARCHAR(255) NOT NULL,
+        FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+    ) default charset utf8 COMMENT '';
+
+DROP TABLE keeps;

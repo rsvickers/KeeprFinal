88b0436ddc5bda84098c8d6b16566da40a18e47d
Rhys Vickers <rs.vickers5@gmail.com>
2023-12-10 17:23:05 -0700

    semi styled

--- client/src/components/VaultCardComponent.vue
+++ client/src/components/VaultCardComponent.vue
@@ -1,7 +1,10 @@
 <template>
-    <div class="component">
-        <!-- TODO make this look pretty and then make it clickable to go on a details page -->
-        <p> {{ vaultProp.name }}</p>
+    <div class="backgroundImg mb-5 mt-4  rounded text-light d-flex" role="button" title="click to see details"
+        :style="{ backgroundImage: `url(${vaultProp?.img})` }">
+        <div class="d-flex align-items-end">
+            <p class="p-2 box rounded"> {{ vaultProp.name }}</p>
+        </div>
+
     </div>
 </template>
 
@@ -24,4 +27,18 @@ export default {
 </script>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.backgroundImg {
+    height: 20rem;
+    width: 100%;
+    background-position: center;
+    background-size: cover;
+    box-shadow: 1px 1px 15px;
+}
+
+.box {
+    // border: solid rgb(255, 255, 255);
+    box-shadow: 1px 1px rgba(215, 215, 215, 0.759);
+    background-color: rgba(38, 38, 38, 0.421);
+}
+</style>

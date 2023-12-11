f889aea5ab0781028a069d0bed1387e39306f44f
Rhys Vickers <rs.vickers5@gmail.com>
2023-12-11 16:04:12 -0700

    I can get vaultkeeps in a vault???

--- client/src/services/VaultKeepsService.js
+++ client/src/services/VaultKeepsService.js
@@ -1,8 +1,15 @@
+import { AppState } from "../AppState"
+import { VaultKeep } from "../models/VaultKeep"
 import { logger } from "../utils/Logger"
+import { api } from "./AxiosService"
 
 class VaultKeepsService {
     async createKeepVault(vaultKeepData) {
-        logger.log('Anything?', vaultKeepData)
+        // logger.log('Anything?', vaultKeepData)
+        const res = await api.post('api/vaultKeeps', vaultKeepData)
+        const newVaultKeep = new VaultKeep(res.data)
+        AppState.vaultKeeps.push(newVaultKeep)
+        return newVaultKeep
     }
 }
 
